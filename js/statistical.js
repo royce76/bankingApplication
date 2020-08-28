a925d3503c8eef88723c26f9e8744da75517b298
royce76 <67342235+royce76@users.noreply.github.com>
2020-08-28 19:19:42 +0200

    add statistic page

--- js/statistical.js
+++ js/statistical.js
@@ -1,4 +1,4 @@
-//Loading
+//Loading figure use to see if request id done
 let loading = document.querySelector(".spinner-border");
 
 //Ask request
@@ -17,7 +17,7 @@ function bourseInformation() {
         if (httpRequest.status === 200) {
             loading.classList.add("d-none");
             let bourse = JSON.parse(httpRequest.response)
-            console.log(bourse);
+            show(bourse);
         } else {
             console.log("en attente");
         }
@@ -25,4 +25,33 @@ function bourseInformation() {
         console.log("en attente");
     }
 }
-
+//function to show details from the variable bourse.
+function show(data) {
+    for(let preciseData in data) {
+        if(preciseData === "Taux Emprunt") {
+            for(let prop in data[preciseData]) {
+                let emprunt = document.createElement("li");
+                document.getElementById("taux").appendChild(emprunt);
+                emprunt.classList.add("list-group-item");
+                emprunt.innerText = prop + ' = ' + data[preciseData][prop] + '.';    
+            } 
+        }
+        else if(preciseData === "Cours de la Bourse") {
+            for(let prop in data[preciseData]) {
+                let bourseData = document.createElement("li");
+                document.getElementById("coursDeLaBourse").appendChild(bourseData);
+                bourseData.classList.add("list-group-item");
+                bourseData.innerText = prop + ' = (' + data[preciseData][prop][0] + ', ' + data[preciseData][prop][1] +').' ;    
+   
+            } 
+        }
+        if(preciseData === "Cours de la monnaie") {
+            for(let prop in data[preciseData]) {
+                let monnaie = document.createElement("li");
+                document.getElementById("CoursDeLaMonnaie").appendChild(monnaie);
+                monnaie.classList.add("list-group-item");
+                monnaie.innerText = prop + ' = ' + data[preciseData][prop] + '.';    
+            } 
+        } 
+    }
+}
